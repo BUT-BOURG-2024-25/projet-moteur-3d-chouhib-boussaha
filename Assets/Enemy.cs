e7a943063fd014b29ec23b62e637596bb55c0071
Ilias <chouhibilias@gmail.com>
2024-12-25 14:32:49 +0100

    killing enemies with color

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,17 +4,38 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float health = 100f;
+    private float currentHealth = 0f;
+
+    private SkinnedMeshRenderer renderer;
+
+
+
+    public void Start()
+    {
+        Transform skinnedMeshChild = transform.Find("BountyHunterRIO2").transform.Find("BountyHunter_RIO_2");
+        if (skinnedMeshChild != null)
+        {
+            renderer = skinnedMeshChild.GetComponent<SkinnedMeshRenderer>();
+        }
+
+        currentHealth = health;
+    }
 
     public void TakeDamage(float damage)
     {
-        Debug.Log("HEALTH : " + health + " DAMAGE : " + damage);
+        currentHealth -= damage;
 
-        health -= damage;
-        if (health <= 0)
+        float healthPercentage = currentHealth / health;
+
+        Color color = new Color(1 - healthPercentage, healthPercentage, 0, 0.75f); // RGBA color based on health
+
+        renderer.material.color = color;
+
+        if (currentHealth <= 0)
         {
             Destroy(gameObject);
             EnnemySpawner.Instance.downEnnemyCount();
-
         }
     }
+
 }
